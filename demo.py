c1587d185c651a38d0620243d93f62030c88f62f
Graham <Graham.Williams@togaware.com>
2019-04-04 09:48:31 +0800

    Implement more generic key file handling.

--- demo.py
+++ demo.py
@@ -3,44 +3,52 @@
 
 import requests
 import json
+import os
 
-from utils import get_key
+from utils import get_key_endpoint
 
-# URLs for anomaly detection with the Anomaly Detector API
-batch_detection_url = "/anomalydetector/v1.0/timeseries/entire/detect"
-latest_point_detection_url = "/anomalydetector/v1.0/timeseries/last/detect"
+# Constants
 
-###############################################
-#### Update or verify the following values. ###
-###############################################
+SERVICE   = "Anomaly Detector"
+KEY_FILE  = os.getcwd() + "/private.txt"
+DATA_FILE = "request-data.json"
 
-subscription_key, endpoint = get_key()
+# URLs for anomaly detection with the Anomaly Detector API.
 
-# Replace with a path to the JSON formatted time series data.
-data_location = "request-data.json"
+BATCH_URL  = "/anomalydetector/v1.0/timeseries/entire/detect"
+LATEST_URL = "/anomalydetector/v1.0/timeseries/last/detect"
 
-###############################################
+# Request subscription key and endpoint from user.
 
-"""
-Sends an anomaly detection request to the Anomaly Detector API. 
-If the request is successful, the JSON response is returned.
-"""
+subscription_key, endpoint = get_key_endpoint(KEY_FILE, SERVICE)
+
+# Send a request.
+
 def send_request(endpoint, url, subscription_key, request_data):
-    headers = {'Content-Type': 'application/json', 'Ocp-Apim-Subscription-Key': subscription_key}
-    response = requests.post(endpoint+url, data=json.dumps(request_data), headers=headers)
+    """Send anomaly detection request to the Anomaly Detector API. 
+
+    If the request is successful, the JSON response is returned.
+    """
+    
+    headers = {'Content-Type': 'application/json',
+               'Ocp-Apim-Subscription-Key': subscription_key}
+    
+    response = requests.post(endpoint+url,
+                             data=json.dumps(request_data),
+                             headers=headers)
+    
     if response.status_code == 200:
         return json.loads(response.content.decode("utf-8"))
     else:
         print(response.status_code)
         raise Exception(response.text)
 
-"""
-Detect anomalies throughout the time series data by submitting it as a batch to the API.
-"""
+# Detect anomalies in the time series.
+    
 def detect_batch(request_data):
     print("Detecting anomalies as a batch")
     # Send the request, and print the JSON result
-    result = send_request(endpoint, batch_detection_url, subscription_key, request_data)
+    result = send_request(endpoint, BATCH_URL, subscription_key, request_data)
     print(json.dumps(result, indent=4))
 
     # Find and display the positions of anomalies in the data set
@@ -51,23 +59,24 @@ def detect_batch(request_data):
         if anomalies[x] == True:
             print (x)
 
-"""
-Detect if the latest data point in the time series is an anomaly.
-"""
+# Detect if the latest data point in the time series is an anomaly.
+
 def detect_latest(request_data):
     print("Determining if latest data point is an anomaly")
     # send the request, and print the JSON result
-    result = send_request(endpoint, latest_point_detection_url, subscription_key, request_data)
+    result = send_request(endpoint, LATEST_URL, subscription_key, request_data)
     print(json.dumps(result, indent=4))
 
 
-# read json time series data from file
-file_handler = open (data_location)
+# Read data from a json time series from file.
+
+file_handler = open(DATA_FILE)
 json_data = json.load(file_handler)
 
-# send the request
+# Send the requests.
+
 detect_batch(json_data)
 detect_latest(json_data)
 
+# Generate a plot to show the time series and the anomalies.
 
-# GENERATE A PLOT TO SHOW THE TIME SERIES AND THE ANOMALIES
--- utils.py
+++ utils.py
@@ -1,59 +1,136 @@
-# Endpoint URL can be found in the Azure portal.  for example:
-# https://westus2.api.cognitive.microsoft.com
+# Utilities to support an Azure MLHub package
 #
+# These will migrate to MLHUB itself over time.
 
 # Import the required libraries.
 
 import os
 import sys
+import readline  # Don't remove !! For prompt of input() to take effect
+import termios
+import tty
 
-# Defaults.
+# Load subscription key and endpoint from file.
 
-KEY_FILE = "private.py"
-DEFAULT_REGION = "westus2"
+def load_key(path):
+    key = None
+    endpoint = None
+    markchar = "'\" \t"
+    with open(path, 'r') as file:
+        for line in file:
+            line = line.strip('\n')
+            pair = line.split('=')
+            if len(pair) == 2:
+                k = pair[0].strip(markchar).lower()
+                v = pair[1].strip(markchar)
+                if k == 'key':
+                    key = v
+                elif k == 'endpoint':
+                    endpoint = v
+            elif not line.startswith('#'):
+                line = line.strip(markchar)
+                if line.startswith('http'):
+                    endpoint = line
+                else:
+                    key = line
+    return key, endpoint
 
-def get_key():
+# Either load key/endpoint from file or ask user and save to file.
+
+def get_key_endpoint(key_file, service="Cognitive Services"):
+    """The user is asked for an Azure subscription key and endpoint. The
+    provided information is saved into a file for future use. The
+    contents of that file is the key and endpoint with the endpoint
+    identified as starting with http:
+
+    a14d1234abcda4f2f6e9f565df34ef24
+    https://westus2.api.cognitive.microsoft.com
+
+    """
+
+    key = None
+
+    # Set up messages.
     
-    subscription_key = None
-    region = DEFAULT_REGION
+    prompt_key = "Please paste your {} subscription key: ".format(service)
+    prompt_endpoint = "Please paste your endpoint: "
 
-    # Prompt the user for the key and region and save into private.py
-    # for future runs of the model. The contents of that file is:
-    #
-    # subscription_key = "a14d...ef24"
-    # region = "southeastasia"
-
-    if os.path.isfile(KEY_FILE) and os.path.getsize(KEY_FILE) != 0:
-        print("""The following file has been found and is assumed to contain
-an Azure Speech Services subscription key and region. We will load 
+    msg_request = """\
+An Azure resource is required to access this service (and to run this command).
+See the README for details of a free subscription. If you have a subscription
+then please paste the key and the endpoint here.
+"""
+    msg_found = """\
+The following file has been found and is assumed to contain an Azure 
+subscription key and endpoint for {}. We will load 
 the file and use this information.
 
-    """ + os.getcwd() + "/" + KEY_FILE)
-        exec(open(KEY_FILE).read())
-    else:
-        print("""An Azure resource is required to access this service (and to run this
-demo). See the README for details of a free subscription. Then you can
-provide the key and the region information here.
-""")
-        sys.stdout.write("Please enter your Speech Services subscription key []: ")
-        subscription_key = input()
+    {}
+""".format(service, key_file)
 
-        sys.stdout.write("Please enter your region [southeastasia]: ")
-        region = input()
-        if len(region) == 0: region = DEFAULT_REGION
-
-        if len(subscription_key) > 0:
-            assert subscription_key
-            ofname = open(KEY_FILE, "w")
-            ofname.write("""subscription_key = "{}"
-region = "{}"
-    """.format(subscription_key, region))
-            ofname.close()
-
-            print("""
+    msg_saved = """
 I've saved that information into the file:
 
-        """ + os.getcwd() + "/" + KEY_FILE)
+    {}
+""".format(key_file)
 
-        endpoint = "https://{}.api.cognitive.microsoft.com".format(region)
-        return(subscription_key, endpoint)
+    # Obtain the key/endpoint.
+    
+    if os.path.isfile(key_file) and os.path.getsize(key_file) > 0:
+        print(msg_found)
+        key, endpoint = load_key(key_file)
+    else:
+        print(msg_request)
+        
+        key      = ask_password(prompt_key)
+        endpoint = input(prompt_endpoint)
+
+        if len(key) > 0 and len(endpoint) > 0:
+            ofname = open(key_file, "w")
+            ofname.write("{}\n{}\n".format(key, endpoint))
+            ofname.close()
+            print(msg_saved)
+
+    return key, endpoint
+
+# Simple input of password.
+
+def ask_password(prompt=None):
+    """Echo '*' for every input character. Only implements the basic I/O
+    functionality and so only Backspace is supported.  No support for
+    Delete, Left key, Right key and any other line editing.
+
+    Reference: https://mail.python.org/pipermail/python-list/2011-December/615955.html
+    """
+
+    symbol = "`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?"
+    if prompt:
+        sys.stdout.write(prompt)
+        sys.stdout.flush()
+
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    chars = []
+    try:
+        tty.setraw(sys.stdin.fileno())
+        while True:
+            c = sys.stdin.read(1)
+            if c in '\n\r':  # Enter.
+                break
+            if c == '\003':
+                raise KeyboardInterrupt
+            if c == '\x7f':  # Backspace.
+                if chars:
+                    sys.stdout.write('\b \b')
+                    sys.stdout.flush()
+                    del chars[-1]
+                continue
+            if c.isalnum() or c in symbol:
+                sys.stdout.write('*')
+                sys.stdout.flush()
+                chars.append(c)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        sys.stdout.write('\n')
+
+    return ''.join(chars)
